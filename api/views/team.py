13aed8dafb195927b6d5d6b2ff1bf87a14ba66de
dubinavlad <72496539+dubinavlad@users.noreply.github.com>
2021-01-15 11:52:15 +0200

    Add files via upload

--- api/views/team.py
+++ api/views/team.py
@@ -1,12 +1,7 @@
 from user_site.models import Team
 from rest_framework import viewsets
-import json
-from rest_framework import status
-from rest_framework.decorators import action, api_view
 from api.serializers.team import TeamSerializer
-from django.views.decorators.csrf import csrf_exempt
 
-from rest_framework.response import Response
 
 
 class TeamView(viewsets.ModelViewSet):
